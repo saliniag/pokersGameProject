676cc5b5ff223135043cacb85213bf51c2c51afb
saliniag <salini.agarwal11@gmail.com>
2017-09-08 11:46:44 -0700

    poker game

---
+++ CardLibrary/SuperCard.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardLibrary
+{
+
+    public enum Suit
+    {
+        Club = 1,
+        Diamond,
+        Heart,
+        Spade
+    }
+
+    public enum Rank
+    {
+        Deuce = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13,
+        Ace = 14
+    }
+    public abstract class SuperCard: IComparable<SuperCard>
+    {
+
+        public Rank CardRank { get; set; }
+        
+        public abstract Suit CardSuit { get; }
+
+        public abstract void Display();
+
+        //implementation of compareTo method
+        public int CompareTo(SuperCard other)
+        {
+            if ((int)this.CardSuit > (int)other.CardSuit) 
+            {
+                return 1;
+                
+            }
+            else if ((int)this.CardSuit < (int)other.CardSuit) 
+            {
+                return -1;
+            }
+            //if ((int)this.CardSuit == (int)other.CardSuit)// && ((int)this.CardRank > (int)other.CardRank))
+            //{
+            //    if ((int)this.CardRank > (int)other.CardRank)
+            //    { return 1; }
+
+            //   if ((int)this.CardRank < (int)other.CardRank) { return -1; }
+
+            //   else { return 0; }
+                
+            //}
+            else { return 0; }
+
+        }
+
+
+    }
+
+    //child classes
+   
+    public class CardClub : SuperCard
+    {
+        private Suit Club_CardSuit = Suit.Club;
+        //overriding CardSuit property
+        public override Suit CardSuit
+        {
+            get { return Club_CardSuit; }
+        }
+
+        public override void Display()
+        {
+            // Code to Display a club card...
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(CardRank + " of " + CardSuit + "s ♣");
+            Console.ResetColor();
+        }
+
+        //constructor
+        public CardClub(Rank myRank)
+        {
+            CardRank = myRank;
+        }
+
+
+    }
+
+    public class CardDiamond : SuperCard {
+        private Suit Diamond_CardSuit = Suit.Diamond;
+
+        //overriding CardSuit property
+        public override Suit CardSuit
+        {
+            get { return Diamond_CardSuit; }
+        }
+
+        public override void Display()
+        {
+            // Code to Display a diamond card...
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(CardRank + " of " + CardSuit + "s ♦");
+            Console.ResetColor();
+        }
+
+
+        //constructor
+        public CardDiamond(Rank myRank)
+        {
+            CardRank = myRank;
+        }
+    }
+
+    public class CardHeart : SuperCard {
+        private Suit Heart_CardSuit = Suit.Heart;
+
+        //overriding CardSuit property
+        public override Suit CardSuit
+        {
+            get { return Heart_CardSuit; }
+        }
+
+
+        public override void Display()
+        {
+            // Code to Display a heart card...
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(CardRank + " of " + CardSuit + "s ♥");
+            Console.ResetColor();
+        }
+
+
+        //constructor
+        public CardHeart(Rank myRank)
+        {
+            CardRank = myRank;
+        }
+        
+    }
+
+    public class CardSpade : SuperCard {
+
+        private Suit Spade_CardSuit = Suit.Spade;
+
+        //overriding CardSuit property
+        public override Suit CardSuit
+        {
+            get { return Spade_CardSuit; }
+        }
+
+        public override void Display()
+        {
+            // Code to Display a Spade card...
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(CardRank + " of " + CardSuit + "s ♠");
+            Console.ResetColor();
+        }
+
+
+        //constructor
+        public CardSpade(Rank myRank)
+        {
+            CardRank = myRank;
+        }
+    }
+
+//end of inherited class spercard
+
+    //creating class CardSet
+
+    public class CardSet
+    {
+        public SuperCard[] cardArray;
+
+        //instant iating random class\
+        Random myRandom = new Random();
+        //constructor
+        public CardSet()
+        {
+            cardArray = new SuperCard[52];
+            
+            
+
+            //looping for CardClub 
+            int index2 = 2;
+            for (int index1 = 0; index1 < 13; index1++)
+            {
+                
+                cardArray[index1] = new CardClub((Rank)(index2));
+                index2 = index2 + 1;
+            }
+
+            //looping for cardDiamond
+            index2 = 2;
+            for (int index1 = 13; index1 < 26; index1++)
+            {
+
+                cardArray[index1] = new CardDiamond((Rank)(index2));
+                index2 = index2 + 1;
+            }
+
+            //looping for cardHeart
+            index2 = 2;
+            for (int index1 = 26; index1 < 39; index1++)
+            {
+
+                cardArray[index1] = new CardHeart((Rank)(index2));
+                index2 = index2 + 1;
+            }
+            //looping for cardSpade
+            index2 = 2;
+            for (int index1 =39 ; index1 < 52; index1++)
+            {
+
+                cardArray[index1] = new CardSpade((Rank)(index2));
+                index2 = index2 + 1;
+            }   
+        }//end of constructor
+
+        public SuperCard[] GetCards(int number)
+        {
+            SuperCard[] oneHand = new SuperCard[number];
+            for (int i = 0; i < number; i++)
+            {
+                int myNum = myRandom.Next(cardArray.Length);
+                oneHand[i] = cardArray[myNum];
+                 
+            }
+            return oneHand; 
+               
+        }
+        
+        
+    }   
+
+}
+ 
---
+++ PokerProject/Program.cs
@@ -0,0 +1,147 @@
+﻿using CardLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // a. Create an instance of your CardSet class, and call it myDeck.
+            CardSet myDeck = new CardSet();
+            // b. Build a loop that does a Console.WriteLine 52 times.
+           
+
+            //creating howmanyCard
+            int howManyCards = 3;
+
+
+            //players balance
+            int balance = 10;
+
+            //setting the screen color
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Welcome to the Poker Game.");
+            Console.WriteLine("you have $10 and each bet will be $1.");
+            Console.WriteLine("Press any key when you are ready to start.");
+
+            Console.ReadLine();
+            while (balance > 0)
+            {
+                //calling the mehods
+                SuperCard[] computerHand = myDeck.GetCards(howManyCards);
+                SuperCard[] playersHand = myDeck.GetCards(howManyCards);
+
+                //sorting the card
+                Array.Sort(computerHand);
+                Array.Sort(playersHand);
+
+
+                //displaying the cards
+                DisplayHands(computerHand, playersHand);
+
+                //setting the colors again
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+                //comparing the cards
+                Console.WriteLine();
+                bool won = CompareHands(computerHand, playersHand);
+
+
+                
+
+                if (won)
+                { Console.WriteLine("Congrats! You won the game");
+                balance += 1;
+                }
+                else
+                { Console.WriteLine("Sorry! You lost the game");
+                balance -= 1;
+                }
+
+                //decreasing the balance
+                Console.WriteLine("your new balance is {0}", balance);
+
+                Console.WriteLine("please press Enter to play another round");
+                Console.ReadLine();
+                
+            }
+            
+            Console.WriteLine("your balance is now zero.");
+            
+
+            // end of program
+            Console.ReadLine();
+
+        }
+        public static void DisplayHands(SuperCard[] computerHand, SuperCard[] playersHand)
+        {
+            //loop for computer hands
+            Console.WriteLine("COMPUTER HANDS");
+            foreach (SuperCard card in computerHand)
+            {
+                card.Display();
+                Console.ResetColor();
+            }
+
+            //loop for players hands
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("PLAYERS HANDS");
+            foreach (SuperCard card in playersHand)
+            {
+                card.Display();
+                Console.ResetColor();
+            }
+
+        }    //end of display method
+
+
+        public static bool CompareHands(SuperCard[] computerHand, SuperCard[] playersHand)
+        {
+            int computerHandTotal = 0;
+            int playersHandTotal = 0;
+            //adding all the ranks of computer hand
+           
+
+            foreach (SuperCard card in computerHand) //see each card
+            {
+                int cardValue;
+                for(int x=0; x<computerHand.Length; x++)  //getting values one by one of each card and add it to computer hand total
+                {
+                    cardValue=  (int)card.CardRank; //getting the value of rank of each card
+                    computerHandTotal += cardValue;
+                }
+               
+            }
+
+            foreach (SuperCard card in playersHand)
+            {
+                int cardValue;
+                for (int x = 0; x <playersHand.Length; x++)
+                {
+                    cardValue = (int)card.CardRank;
+                    playersHandTotal += cardValue;
+                }
+
+            }
+
+            //now compare
+            if (playersHandTotal > computerHandTotal)
+            {
+                return true;
+            }
+            else { return false; }
+
+
+        }//end of compare method
+
+    }
+}
